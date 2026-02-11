b284b6fdd8b2de89cb3e4a36ca4ae43a652ea1ff
vaizhnavibabu <vaishnavibabu06vb@gmail.com>
2026-02-11 22:49:54 +0530

    file is updated for linear regression

--- gain.py
+++ gain.py
@@ -1,24 +1,6 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import linear_model
-
-df=pd.read_csv("homeprice.csv")
-df
-
-%matplotlib inline
-plt.xlabel('year')
-plt.ylabel('price(US$)')
-plt.scatter(df.area,df.price,color='red',marker='+')
-
-reg=linear_model.LinearRegression()
-reg.fit(df[['year']],df.price)
-
-reg.predict(3300)
-
-reg.coeff
-
-reg.intercept
-
-y=m*x+b
-print(y)
+print("linear regression format")
+print("formula: y=mx+c")
+m=20
+x=50
+c=28
+print("y=",m*x+c)
