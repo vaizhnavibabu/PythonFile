7cf03c4453df799e24b6618a3b58ca80dc6cddd5
vaizhnavibabu <vaishnavibabu06vb@gmail.com>
2026-02-12 23:08:48 +0530

    area of the rectangle given

--- gain.py
+++ gain.py
@@ -1,1 +1,15 @@
+# area of rectangle
+
+a=int(input("Give the value for length:"))
+b=int(input("Give the value for breath:"))
+if(a>b):
+    result = a*b
+    print("the area of rectangle is:",result)
+elif(a=b):
+    result = a*a
+    print("the area of square:",result)
+else:
+    print("there is no breath is greater than legth")
+
+print("finally it is printed successfully")
 
